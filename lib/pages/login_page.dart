1072c3e578b849e97c11fad9cea6446c26f88479
xjeansantana <jean.santtana@gmail.com>
2020-01-04 16:50:03 -0300

    Layout do Form de Login (básico)

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,6 +11,39 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  _body(){
+  _body() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: ListView(
+        children: <Widget>[
+          Text("Login"),
+          TextFormField(),
+          SizedBox(
+            height: 10,
+          ),
+          Text("Senha"),
+          TextFormField(
+            obscureText: true,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 46,
+            child: RaisedButton(
+              color: Colors.blue,
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
