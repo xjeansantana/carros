430cc687a844fae572647036b45ce33283133472
xjeansantana <jean.santtana@gmail.com>
2020-01-04 16:33:55 -0300

    Criando a estrutura básica do projeto

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body(){
+  }
+}
