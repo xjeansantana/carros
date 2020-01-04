6b672d87534110bddeced12d331160a7a06c9400
xjeansantana <jean.santtana@gmail.com>
2020-01-04 17:21:21 -0300

    Controlando o Teclado (TextInputType e TextInputAction)

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _tLogin = TextEditingController(text: "ricardo");
+
+  final _tSenha = TextEditingController(text: "123");
+
+  final _focusSenha = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,65 +30,123 @@ class LoginPage extends StatelessWidget {
   }
 
   _body() {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: ListView(
-        children: <Widget>[
-          TextFormField(
-            keyboardType: TextInputType.text,
-            style: TextStyle(
-                color: Colors.blue,
-                fontSize: 25
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            _text("Login", "Digite o login",
+                controller: _tLogin,
+                validator: _validateLogin,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                nextFocus: _focusSenha),
+            SizedBox(height: 10),
+            _text("Senha", "Digite a senha",
+                controller: _tSenha,
+                password: true,
+                validator: _validateSenha,
+                keyboardType: TextInputType.number,
+                focusNode: _focusSenha),
+            SizedBox(
+              height: 20,
             ),
-            decoration: InputDecoration(
-                labelText: "Login",
-                labelStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 25
-                ),
-                hintText: "Digite o login",
-                hintStyle: TextStyle(
-                    fontSize: 18
-                )
-            ),
-          ),
-          SizedBox(height: 10,),
-          TextFormField(
-            obscureText: true,
-            keyboardType: TextInputType.number,
-            style: TextStyle(
-                color: Colors.blue,
-                fontSize: 25
-            ),
-            decoration: InputDecoration(
-                labelText: "Senha",
-                labelStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 25
-                ),
-                hintText: "Digite a senha",
-                hintStyle: TextStyle(
-                    fontSize: 18
-                )
-            ),
-          ),
-          SizedBox(height: 20,),
-          Container(
-            height: 46,
-            child: RaisedButton(
-              color: Colors.blue,
-              child: Text(
-                "Login",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                ),
-              ),
-              onPressed: () {},
-            ),
-          )
-        ],
+            _button("Login", _onClickLogin),
+          ],
+        ),
       ),
     );
+  }
+
+  _text(
+    String label,
+    String hint, {
+    bool password = false,
+    TextEditingController controller,
+    FormFieldValidator<String> validator,
+    TextInputType keyboardType,
+    TextInputAction textInputAction,
+    FocusNode focusNode,
+    FocusNode nextFocus,
+  }) {
+    return TextFormField(
+      controller: controller,
+      obscureText: password,
+      validator: validator,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      focusNode: focusNode,
+      onFieldSubmitted: (String text) {
+        if (nextFocus != null) {
+          FocusScope.of(context).requestFocus(nextFocus);
+        }
+      },
+      style: TextStyle(
+        fontSize: 25,
+        color: Colors.blue,
+      ),
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(
+          fontSize: 25,
+          color: Colors.grey,
+        ),
+        hintText: hint,
+        hintStyle: TextStyle(
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+
+  _button(String text, Function onPressed) {
+    return Container(
+      height: 46,
+      child: RaisedButton(
+        color: Colors.blue,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+        ),
+        onPressed: onPressed,
+      ),
+    );
+  }
+
+  void _onClickLogin() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+
+    String login = _tLogin.text;
+    String senha = _tSenha.text;
+
+    print("Login: $login, Senha: $senha");
+  }
+
+  String _validateLogin(String text) {
+    if (text.isEmpty) {
+      return "Digite o login";
+    }
+    return null;
+  }
+
+  String _validateSenha(String text) {
+    if (text.isEmpty) {
+      return "Digite a senha";
+    }
+    if (text.length < 3) {
+      return "A senha precisa ter pelo menos 3 números";
+    }
+    return null;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
