0912571fc38f4e3124b9822851fd57ce2862f704
xjeansantana <jean.santtana@gmail.com>
2020-01-14 21:42:23 -0300

    CarrosListView

--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:carros/drawer_list.dart.dart';
-import 'package:carros/pages/carro/carro.dart';
-import 'package:carros/pages/carro/carros_api.dart';
+import 'package:carros/pages/carro/carros_listview.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,99 +9,8 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Carros"),
       ),
-      body: _body(),
+      body: CarrosListView(),
       drawer: DrawerList(),
-    );
-  }
-
-  _body() {
-    Future<List<Carro>> future = CarrosApi.getCarros();
-
-    return FutureBuilder(
-      future: future,
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.hasError) {
-          print(snapshot.error);
-          return Center(
-            child: Text(
-              "Não foi possível listar os carros",
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 22,
-              ),
-            ),
-          );
-        }
-
-        if (!snapshot.hasData) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-
-        List<Carro> carros = snapshot.data;
-        return _listView(carros);
-      },
-    );
-  }
-
-  Container _listView(List<Carro> carros) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: ListView.builder(
-        itemCount: carros != null ? carros.length : 0,
-        itemBuilder: (context, index) {
-          Carro carro = carros[index];
-
-          return Card(
-            color: Colors.grey[100],
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Center(
-                    child: Image.network(
-                      carro.urlFoto,
-                      width: 250,
-                    ),
-                  ),
-                  Text(
-                    carro.nome,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
-                  Text(
-                    "descrição...",
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                  ButtonBar(
-                    children: <Widget>[
-                      FlatButton(
-                        child: const Text('DETALHES'),
-                        onPressed: () {
-                          /* ... */
-                        },
-                      ),
-                      FlatButton(
-                        child: const Text('SHARE'),
-                        onPressed: () {
-                          /* ... */
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
     );
   }
 }
