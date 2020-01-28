dd06b4c57ef518797f1d661a016c05ed4724db23
xjeansantana <jean.santtana@gmail.com>
2020-01-28 19:21:30 -0300

    TabBar - listener

--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -9,33 +9,47 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>with SingleTickerProviderStateMixin<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin<HomePage> {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _tabController = TabController(length: 3, vsync: this);
+
+    _tabController.index = 1;
+
+    _tabController.addListener(() {
+      print("Tab ${_tabController.index}");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Carros"),
-          bottom: TabBar(tabs: [
-            Tab(
-              text: "Clássicos",
-            ),
-            Tab(
-              text: "Espotivos",
-            ),
-            Tab(
-              text: "Luxo",
-            ),
-          ]),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: "Clássicos"),
+            Tab(text: "Espotivos"),
+            Tab(text: "Luxo"),
+          ],
         ),
-        body: TabBarView(children: [
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
           CarrosListView(TipoCarro.classicos),
           CarrosListView(TipoCarro.esportivos),
           CarrosListView(TipoCarro.luxo),
-        ]),
-        drawer: DrawerList(),
+        ],
       ),
+      drawer: DrawerList(),
     );
   }
 }
