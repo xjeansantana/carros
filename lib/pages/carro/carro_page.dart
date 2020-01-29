b5cb1ccf04ba335219d965b30bce4bcfa9a79e07
xjeansantana <jean.santtana@gmail.com>
2020-01-29 20:46:24 -0300

    Carro Detalhes - layout parte 2 - scroll

--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -52,46 +52,75 @@ class CarroPage extends StatelessWidget {
       child: ListView(
         children: <Widget>[
           Image.network(carro.urlFoto),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  text(
-                    carro.nome,
-                    fontSize: 20,
-                    bold: true,
-                  ),
-                  text(
-                    carro.tipo,
-                    fontSize: 16,
-                  ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(
-                      Icons.favorite,
-                      color: Colors.red,
-                      size: 40,
-                    ),
-                    onPressed: _cnClickFavorito,
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.share,
-                      size: 40,
-                    ),
-                    onPressed: _cnClickShare,
-                  ),
-                ],
-              )
-            ],
-          )
+          _bloco1(),
+          Divider(),
+          _bloco2()
         ],
       ),
+    );
+  }
+
+  Row _bloco1() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            text(
+              carro.nome,
+              fontSize: 20,
+              bold: true,
+            ),
+            text(
+              carro.tipo,
+              fontSize: 16,
+            ),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.red,
+                size: 40,
+              ),
+              onPressed: _cnClickFavorito,
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.share,
+                size: 40,
+              ),
+              onPressed: _cnClickShare,
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  _bloco2() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(
+          height: 20,
+        ),
+        text(
+          carro.descricao,
+          fontSize: 16,
+          bold: true,
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        text(
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          fontSize: 16,
+        ),
+      ],
     );
   }
 
