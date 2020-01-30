f31c5d7bd2db31f9ca143ec00e5b360f61669707
xjeansantana <jean.santtana@gmail.com>
2020-01-29 21:27:06 -0300

    Cache - LoripsumApi

--- lib/pages/carro/loripsum_api.dart
+++ lib/pages/carro/loripsum_api.dart
@@ -3,20 +3,25 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 
 class LoripsumBloc {
+
+  static String lorim;
+
   final _streamController = StreamController<String>();
 
   Stream<String> get stream => _streamController.stream;
 
   fetch() async {
-  String s = await LoripsumApi.getLoripsum();
-      _streamController.add(s);
-    }
+    String s = lorim ?? await LoripsumApi.getLoripsum();
+
+    lorim = s;
+
+    _streamController.add(s);
+  }
 
   void dispose() {
     _streamController.close();
   }
 }
-
 
 class LoripsumApi {
   static Future<String> getLoripsum() async {
