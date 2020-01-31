bf7c66b425c01c10a20a37fc9861367f3af5ea04
xjeansantana <jean.santtana@gmail.com>
2020-01-30 22:30:44 -0300

    Assets SQL

--- lib/pages/favoritos/db_helper.dart.dart
+++ lib/pages/favoritos/db_helper.dart.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 
+import 'package:flutter/services.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper _instance = DatabaseHelper.getInstance();
+
   DatabaseHelper.getInstance();
 
   factory DatabaseHelper() => _instance;
@@ -25,20 +27,27 @@ class DatabaseHelper {
     String path = join(databasesPath, 'carros.db');
     print("db $path");
 
-    var db = await openDatabase(path, version: 2, onCreate: _onCreate, onUpgrade: _onUpgrade);
+    var db = await openDatabase(path,
+        version: 2, onCreate: _onCreate, onUpgrade: _onUpgrade);
     return db;
   }
 
   void _onCreate(Database db, int newVersion) async {
-    await db.execute(
-        'CREATE TABLE carro(id INTEGER PRIMARY KEY, tipo TEXT, nome TEXT'
-        ', descricao TEXT, urlFoto TEXT, urlVideo TEXT, latitude TEXT, longitude TEXT)');
+    String s = await rootBundle.loadString("assets/sql/create.sql");
+    List<String> sqls = s.split(";");
+
+    for (String sql in sqls)
+      if (sql.trim().isNotEmpty) {
+        print("sql: $sql");
+        await db.execute(sql);
+      }
   }
 
-  Future<FutureOr<void>> _onUpgrade(Database db, int oldVersion, int newVersion) async {
+  Future<FutureOr<void>> _onUpgrade(
+      Database db, int oldVersion, int newVersion) async {
     print("_onUpgrade: oldVersion: $oldVersion > newVersion: $newVersion");
 
-    if(oldVersion == 1 && newVersion == 2) {
+    if (oldVersion == 1 && newVersion == 2) {
       await db.execute("alter table carro add column NOVA TEXT");
     }
   }
